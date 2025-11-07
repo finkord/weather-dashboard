ae3d1ddbbce9fd72019e72c7fcf061f145784735
finkord <devfinkord@gmail.com>
2025-11-07 22:10:11 +0200

    components/sidebar transfer on material

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+// 1. Імпортуємо необхідні модулі Material
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
